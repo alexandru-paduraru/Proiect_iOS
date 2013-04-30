8cf2eed5e17b6654ca83cc17312df7107c552ec8
Axelut Alex <Axelut@Axeluts-MacBook-Pro.local>
2013-04-30 17:30:14 +0300

    asd
    asd

---
+++ Proiect_iOS/EventsTableVC.h
@@ -0,0 +1,13 @@
+//
+//  EventsTableVC.h
+//  Proiect_iOS
+//
+//  Created by Axelut Alex on 214//13.
+//  Copyright (c) 2013 Axelut Alex. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface EventsTableVC : PFQueryTableViewController
+
+@end
---
+++ Proiect_iOS/EventsTableVC.m
@@ -0,0 +1,38 @@
+//
+//  EventsTableVC.m
+//  Proiect_iOS
+//
+//  Created by Axelut Alex on 214//13.
+//  Copyright (c) 2013 Axelut Alex. All rights reserved.
+//
+
+#import "EventsTableVC.h"
+
+@interface EventsTableVC ()
+
+@end
+
+@implementation EventsTableVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
