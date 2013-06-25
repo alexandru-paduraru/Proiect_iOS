461b7b8982c84d570b934aeafcb6bb450f11f5ef
Axelut Alex <Axelut@Axeluts-MacBook-Pro.local>
2013-06-25 15:42:40 +0300

    few
    wef

---
+++ Proiect_iOS/ChooseCoverVC.h
@@ -0,0 +1,13 @@
+//
+//  ChooseCoverVC.h
+//  Proiect_iOS
+//
+//  Created by Axelut Alex on 196//13.
+//  Copyright (c) 2013 Axelut Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChooseCoverVC : UIViewController
+
+@end
---
+++ Proiect_iOS/EventDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventDetailsViewController.h
+//  Proiect_iOS
+//
+//  Created by Axelut Alex on 135//13.
+//  Copyright (c) 2013 Axelut Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventDetailsViewController : UIViewController
+
+@end
